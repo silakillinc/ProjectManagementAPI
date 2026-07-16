f24fa66b4375b978ace72e030b812e2a0c5fdaaf
Sıla Kılınç <silakilinc@Sla-MacBook-Air.local>
2026-07-16 23:02:25 +0300

    Veritabani kisitlari ve iliskileri eklendi

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -16,5 +16,119 @@ namespace ProjectManagement.API
         public DbSet<ProjectMember> ProjectMembers { get; set; }
         public DbSet<TaskHistory> TaskHistories { get; set; }
         public DbSet<TaskTimeLog> TaskTimeLogs { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<User>()
+        .HasIndex(u => u.Email)
+        .IsUnique();
+
+        modelBuilder.Entity<User>()
+        .Property(u => u.Email)
+        .UseCollation("NOCASE");
+
+        modelBuilder.Entity<ProjectMember>()
+        .HasIndex(pm => new { pm.ProjectId, pm.UserId })
+        .IsUnique();
+
+        modelBuilder.Entity<User>()
+        .Property(u => u.FirstName)
+        .HasMaxLength(50)
+        .IsRequired();
+
+        modelBuilder.Entity<User>()
+        .Property(u => u.LastName)
+        .HasMaxLength(50)
+        .IsRequired();
+
+        modelBuilder.Entity<User>()
+        .Property(u => u.Email)
+        .HasMaxLength(200)
+        .IsRequired()
+        .UseCollation("NOCASE");
+
+        modelBuilder.Entity<User>()
+        .Property(u => u.PasswordHash)
+        .IsRequired();
+
+        modelBuilder.Entity<User>()
+        .Property(u => u.Department)
+        .HasMaxLength(100);
+
+        modelBuilder.Entity<Project>()
+        .Property(p => p.Name)
+        .HasMaxLength(200)
+        .IsRequired();
+
+        modelBuilder.Entity<ProjectTask>()
+        .Property(t => t.Title)
+        .HasMaxLength(200)
+        .IsRequired();
+
+        modelBuilder.Entity<Comment>()
+        .Property(c => c.Content)
+        .IsRequired();
+
+        modelBuilder.Entity<TaskTimeLog>()
+        .Property(tl => tl.Description)
+        .HasMaxLength(500);
+
+        modelBuilder.Entity<ProjectTask>()
+        .Property(t => t.EstimatedHours)
+        .HasPrecision(8, 2);
+
+        modelBuilder.Entity<TaskTimeLog>()
+        .Property(tl => tl.Hours)
+        .HasPrecision(8, 2);
+
+        modelBuilder.Entity<Project>()
+        .HasOne(p => p.Owner)
+        .WithMany()
+        .HasForeignKey(p => p.OwnerId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<ProjectMember>()
+        .HasOne(pm => pm.Project)
+        .WithMany()
+        .HasForeignKey(pm => pm.ProjectId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<ProjectMember>()
+        .HasOne(pm => pm.User)
+        .WithMany()
+        .HasForeignKey(pm => pm.UserId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<ProjectTask>()
+        .HasOne(t => t.Project)
+        .WithMany()
+        .HasForeignKey(t => t.ProjectId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<ProjectTask>()
+        .HasOne(t => t.AssignedToUser)
+        .WithMany()
+        .HasForeignKey(t => t.AssignedToUserId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<ProjectTask>()
+        .HasOne(t => t.CreatedByUser)
+        .WithMany()
+        .HasForeignKey(t => t.CreatedByUserId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Comment>()
+        .HasOne(c => c.Task)
+        .WithMany()
+        .HasForeignKey(c => c.TaskId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Comment>()
+        .HasOne(c => c.User)
+        .WithMany()
+        .HasForeignKey(c => c.UserId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+    }
     }
 }
---
+++ Migrations/20260716194321_AddDatabaseConstraints.cs
@@ -0,0 +1,268 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectManagement.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDatabaseConstraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Tasks_TaskId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Users_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectMembers_Projects_ProjectId",
+                table: "ProjectMembers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectMembers_Users_UserId",
+                table: "ProjectMembers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Users_OwnerId",
+                table: "Projects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Projects_ProjectId",
+                table: "Tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_AssignedToUserId",
+                table: "Tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_CreatedByUserId",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectMembers_ProjectId",
+                table: "ProjectMembers");
+
+            migrationBuilder.RenameColumn(
+                name: "isDeleted",
+                table: "Projects",
+                newName: "IsDeleted");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "TEXT",
+                maxLength: 200,
+                nullable: false,
+                collation: "NOCASE",
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectMembers_ProjectId_UserId",
+                table: "ProjectMembers",
+                columns: new[] { "ProjectId", "UserId" },
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Tasks_TaskId",
+                table: "Comments",
+                column: "TaskId",
+                principalTable: "Tasks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Users_UserId",
+                table: "Comments",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectMembers_Projects_ProjectId",
+                table: "ProjectMembers",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectMembers_Users_UserId",
+                table: "ProjectMembers",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Users_OwnerId",
+                table: "Projects",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Projects_ProjectId",
+                table: "Tasks",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_AssignedToUserId",
+                table: "Tasks",
+                column: "AssignedToUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_CreatedByUserId",
+                table: "Tasks",
+                column: "CreatedByUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Tasks_TaskId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Users_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectMembers_Projects_ProjectId",
+                table: "ProjectMembers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectMembers_Users_UserId",
+                table: "ProjectMembers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Users_OwnerId",
+                table: "Projects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Projects_ProjectId",
+                table: "Tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_AssignedToUserId",
+                table: "Tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_CreatedByUserId",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectMembers_ProjectId_UserId",
+                table: "ProjectMembers");
+
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "Projects",
+                newName: "isDeleted");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 200,
+                oldCollation: "NOCASE");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectMembers_ProjectId",
+                table: "ProjectMembers",
+                column: "ProjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Tasks_TaskId",
+                table: "Comments",
+                column: "TaskId",
+                principalTable: "Tasks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Users_UserId",
+                table: "Comments",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectMembers_Projects_ProjectId",
+                table: "ProjectMembers",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectMembers_Users_UserId",
+                table: "ProjectMembers",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Users_OwnerId",
+                table: "Projects",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Projects_ProjectId",
+                table: "Tasks",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_AssignedToUserId",
+                table: "Tasks",
+                column: "AssignedToUserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_CreatedByUserId",
+                table: "Tasks",
+                column: "CreatedByUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
